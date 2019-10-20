5961e0f1fb25c09c8934e560a171ea56aa432c6a
S3rgy53rgy <48632348+S3rgy53rgy@users.noreply.github.com>
2019-10-20 23:08:52 +0300

    Updates

---
+++ precache-manifest.3f14b128ca59ea10f2b0f7a9f8f666f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93655add7b669660b619adceaf25667a",
+    "url": "/landmarks/index.html"
+  },
+  {
+    "revision": "2f0576c4d6e89570acb3",
+    "url": "/landmarks/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "2f0576c4d6e89570acb3",
+    "url": "/landmarks/static/js/2.5312dcb9.chunk.js"
+  },
+  {
+    "revision": "03fd0e3757b245c79e6c",
+    "url": "/landmarks/static/js/main.f0ba5bb9.chunk.js"
+  },
+  {
+    "revision": "a15742dba7bf51fdbb79",
+    "url": "/landmarks/static/js/runtime-main.aedc6b09.js"
+  }
+]);
